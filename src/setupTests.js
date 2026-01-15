b71893ad411d1924b5715ba9e3cb3bacf5658c11
David Rosser <davidrosser92@gmail.com>
2026-01-14 23:27:28 -0600

    fix(ci): remove non-existent package mocks from setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,19 +1,8 @@
-// Polyfill TextEncoder/TextDecoder for dependencies and mock thirdweb for tests.
-
+// Polyfill TextEncoder/TextDecoder for test environment
 const { TextEncoder, TextDecoder } = require('util');
-
 if (typeof global.TextEncoder === 'undefined') {
   global.TextEncoder = TextEncoder;
 }
 if (typeof global.TextDecoder === 'undefined') {
   global.TextDecoder = TextDecoder;
 }
-
-// Mock thirdweb packages that pull in heavy browser deps in Jest.
-jest.mock('@thirdweb-dev/react', () => ({
-  ThirdwebProvider: ({ children }) => children,
-}));
-jest.mock('@thirdweb-dev/chains', () => ({
-  Polygon: {},
-  Mumbai: {},
-}));
